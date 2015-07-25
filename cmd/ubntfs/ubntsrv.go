d8a127ae4ddf6c10685d548fb75c7e5ac0d05d65
William Waites <wwaites@tardis.ed.ac.uk>
2015-07-25 22:17:56 +0100

    add ubnt filesystem with aflist

---
+++ cmd/ubntfs/ubntsrv.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"hubs.net.uk/sw/nopfs"
+	"hubs.net.uk/sw/nopfs/ubnt"
+	"log"
+	)
+
+var addr = flag.String("addr", ":5641", "network address")
+var debug = flag.Int("debug", 0, "print debug messages")
+
+func main() {
+	flag.Parse()
+
+	sfs := new(nopfs.NopSrv)
+	sfs.Debuglevel = *debug
+	sfs.Root = ubnt.Dir
+	sfs.Start(sfs)
+	err := sfs.StartNetListener("tcp", *addr)
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+}
--- ubnt/ubnt.go
+++ ubnt/ubnt.go
@@ -1,1 +1,81 @@
 package ubnt
+
+import (
+	"bytes"
+	"hubs.net.uk/sw/nopfs"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+	"time"
+)
+
+func fixture() (data []byte, err error) {
+	file, err := os.Open("aflist.txt") // For read access.
+	if err != nil {
+		return
+	}
+	data, err = ioutil.ReadAll(file)
+	file.Close()
+	return
+}
+
+var aflist_re *regexp.Regexp
+var aflist_pat string =
+	`^  (?P<k>[^ ][^.]+)\.+(?P<v>[^.].*)[ \t]*`
+var aflist_prog string
+
+func aflist_update(data []byte) {
+	AfList.Lock()
+	defer AfList.Unlock()
+	lines := bytes.Split(data, []byte("\n"))
+	for _, line := range lines {
+		m := aflist_re.FindSubmatch(line)
+		if m != nil {
+			k := m[1]
+			v := m[2]
+//			AfList.AppendUnsafe(string(k), nopfs.NewFile(v))
+			if bytes.HasPrefix(k, []byte("rxpower")) {
+				AfList.AppendUnsafe(string(k), nopfs.NewFile(v))
+			}
+		}
+		
+	}
+	AfList.AppendUnsafe("all", nopfs.NewFile(data))
+}
+
+
+func aflist(tick *time.Ticker) {
+	for _ = range tick.C {
+		cmd := exec.Command(aflist_prog)
+		data, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Printf("aflist: %s", err)
+		} else {
+			aflist_update(data)
+		}
+	}
+}
+
+var AfList *nopfs.Dir
+var Dir *nopfs.Dir
+
+func init() {
+	aflist_re = regexp.MustCompile(aflist_pat)
+
+	Dir = nopfs.NewDir()
+
+	var err error
+	aflist_prog, err = exec.LookPath("aflist")
+	if err == nil {
+		AfList = nopfs.NewDir()
+		Dir.Append("aflist", AfList)
+
+		go func() {
+			ticker := time.NewTicker(5 * time.Minute)
+			aflist(ticker)
+		}()
+	}
+}
+
